71afdb646eedb0709b14db444d3dca4ac3cc67b1
Anderson Pino <anderson.pino2@gmail.com>
2025-07-16 08:50:13 -0500

    actializado

--- views/compras_view.py
+++ views/compras_view.py
@@ -5,6 +5,7 @@ from modules.clientes import obtener_clientes_activos
 from modules.productos import obtener_productos_disponibles
 from modules.compras import registrar_compra
 
+
 def mostrar_registro_compra():
     st.title("🛒 Registrar nueva compra")
 
@@ -19,21 +20,22 @@ def mostrar_registro_compra():
     cliente_id = int(seleccion_cliente.split(" - ")[0])
 
     # Obtener productos disponibles
+    # Obtener productos disponibles
     productos = obtener_productos_disponibles()
     if productos.empty:
         st.warning("⚠️ No hay productos disponibles.")
-        return
+    return
 
-    lista_productos = [
-        f"{id} - {marca} | {nombre} ({genero.capitalize()})"
-       for id, marca, nombre, genero in productos
-    ]
-    
-    seleccion_producto = st.selectbox("Selecciona el producto comprado:", lista_productos)
-    producto_nombre = seleccion_producto.split(" - ", 1)[1]  # Extrae la parte "Marca | Nombre (Género)"
 
-    valor = st.number_input("Valor del producto", min_value=0.0, step=1000.0)
+lista_productos = [
+    f"{fila['id']} - {fila['marca']} | {fila['nombre_producto']} ({fila['genero'].capitalize()})"
+    for _, fila in productos.iterrows()
+]
+seleccion_producto = st.selectbox("Selecciona el producto comprado:", lista_productos)
+producto_nombre = seleccion_producto.split(" - ", 1)[1]
+# Extrae la parte "Marca | Nombre (Género)"
+valor = st.number_input("Valor del producto", min_value=0.0, step=1000.0)
 
-    if st.button("💾 Guardar compra"):
-        if registrar_compra(cliente_id, producto_nombre, valor):
-            st.success("✅ Compra registrada con éxito.")
+if st.button("💾 Guardar compra"):
+    if registrar_compra(cliente_id, producto_nombre, valor):
+        st.success("✅ Compra registrada con éxito.")
