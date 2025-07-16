1326026ff78501d75914ddab3d5e013f7410b242
Anderson Pino <anderson.pino2@gmail.com>
2025-07-16 07:04:31 -0500

    actializado

--- db.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-def conectar():
-    return psycopg2.connect(
-        host="localhost",
-        database="gestor_contactos",
-        user="postgres",
-        password="1030622188"
-    )
