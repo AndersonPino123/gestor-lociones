e3db6a55f7342b08ef8688a5c8ee9935f66effbb
Anderson Pino <anderson.pino2@gmail.com>
2025-07-17 07:48:54 -0500

    actualizado

--- views/graficos.py
+++ views/graficos.py
@@ -4,16 +4,19 @@ import pandas as pd
 import streamlit as st
 from database.connection import obtener_conexion_cursor
 
+
 def mostrar_grafico_lineas():
     st.title("📈 Gráfico de Ventas por Fecha")
     try:
         conexion, cursor = obtener_conexion_cursor()
-        cursor.execute("""
+        cursor.execute(
+            """
             SELECT fecha, SUM(valor) AS total
             FROM compras
             GROUP BY fecha
             ORDER BY fecha;
-        """)
+        """
+        )
         datos = cursor.fetchall()
         conexion.close()
 
@@ -25,3 +28,57 @@ def mostrar_grafico_lineas():
             st.info("No hay datos de ventas para graficar.")
     except Exception as e:
         st.error(f"❌ Error al generar el gráfico: {e}")
+
+
+def mostrar_grafico_barras():
+    st.title("📊 Total de compras por cliente")
+    try:
+        conexion, cursor = obtener_conexion_cursor()
+        cursor.execute(
+            """
+            SELECT c.nombre, COUNT(co.id) 
+            FROM clientes c
+            JOIN compras co ON c.id = co.cliente_id
+            GROUP BY c.nombre
+            ORDER BY COUNT(co.id) DESC
+        """
+        )
+        datos = cursor.fetchall()
+        conexion.close()
+
+        if datos:
+            df = pd.DataFrame(datos, columns=["Cliente", "Compras"])
+            st.bar_chart(df.set_index("Cliente"))
+        else:
+            st.info("No hay datos de compras para graficar.")
+    except Exception as e:
+        st.error(f"❌ Error al generar el gráfico de barras: {e}")
+
+
+def mostrar_grafico_torta():
+    st.title("🍰 Porcentaje de gasto por cliente")
+    try:
+        conexion, cursor = obtener_conexion_cursor()
+        cursor.execute(
+            """
+            SELECT c.nombre, SUM(co.valor)
+            FROM clientes c
+            JOIN compras co ON c.id = co.cliente_id
+            GROUP BY c.nombre
+            ORDER BY SUM(co.valor) DESC;
+        """
+        )
+        datos = cursor.fetchall()
+        conexion.close()
+
+        if datos:
+            df = pd.DataFrame(datos, columns=["Cliente", "Gasto"])
+            st.plotly_chart(
+                st.plotly_pie_chart(
+                    df, names="Cliente", values="Gasto", title="Distribución de gasto"
+                )
+            )
+        else:
+            st.info("No hay datos de gasto para graficar.")
+    except Exception as e:
+        st.error(f"❌ Error al generar el gráfico de torta: {e}")
