e91016c86b24dea5aa07b296b83d0ade5895c980
Anderson Pino <anderson.pino2@gmail.com>
2025-07-17 07:33:00 -0500

    actualizado

--- views/roles_view.py
+++ views/roles_view.py
@@ -3,17 +3,21 @@
 import streamlit as st
 from database.connection import obtener_conexion_cursor
 
+
 def mostrar_gestion_roles(usuario_actual):
     st.title("🔁 Cambiar rol de usuarios")
 
     try:
         conexion, cursor = obtener_conexion_cursor()
-        cursor.execute("""
+        cursor.execute(
+            """
             SELECT id, nombre, correo, rol
             FROM usuarios
             WHERE autorizado = true AND correo != %s
             ORDER BY nombre
-        """, (usuario_actual["correo"],))  # Evita mostrar al usuario actual
+        """,
+            (usuario_actual["correo"],),
+        )  # Evita mostrar al usuario actual
 
         usuarios = cursor.fetchall()
         conexion.close()
@@ -24,17 +28,24 @@ def mostrar_gestion_roles(usuario_actual):
                     nuevo_rol = st.selectbox(
                         "Selecciona el nuevo rol:",
                         ["cliente", "empleado", "administrador"],
-                        index=["cliente", "empleado", "administrador"].index(rol_actual),
-                        key=f"rol_{uid}"
+                        index=["cliente", "empleado", "administrador"].index(
+                            rol_actual
+                        ),
+                        key=f"rol_{uid}",
                     )
                     if nuevo_rol != rol_actual:
                         if st.button("💾 Cambiar rol", key=f"guardar_rol_{uid}"):
                             try:
                                 conn2, cur2 = obtener_conexion_cursor()
-                                cur2.execute("UPDATE usuarios SET rol = %s WHERE id = %s", (nuevo_rol, uid))
+                                cur2.execute(
+                                    "UPDATE usuarios SET rol = %s WHERE id = %s",
+                                    (nuevo_rol, uid),
+                                )
                                 conn2.commit()
                                 conn2.close()
-                                st.success(f"✅ Rol actualizado: {nombre} ahora es {nuevo_rol}.")
+                                st.success(
+                                    f"✅ Rol actualizado: {nombre} ahora es {nuevo_rol}."
+                                )
                                 st.rerun()
                             except Exception as e:
                                 st.error(f"❌ Error al cambiar rol: {e}")
